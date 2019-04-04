b90ab2a0e69e754e248d82bfc0664dd5231b659d
wlm@5203 <ksblk001@126.com>
2019-04-04 16:32:24 +0800

    4.4 模板法实现各模块

--- pages/asr/asr.js
+++ pages/asr/asr.js
@@ -1,4 +1,6 @@
 var app = getApp();
+var util = require('../../utils/util.js');
+
 //微信小程序新录音接口，录出来的是aac或者mp3，这里要录成mp3
 const mp3Recorder = wx.getRecorderManager()
 const mp3RecoderOptions = {
@@ -20,7 +22,10 @@ Page({
     errorMsg:'',
     recoderflag:false,
     startY:0,
-    currentY:0
+    currentY:0,
+    isAdmin: false,
+    isEdit: false,
+    editImage: "../../image/edit.png",
   },
   onLoad: function () {
     wx.showShareMenu({
@@ -54,7 +59,7 @@ Page({
       windowHeight:app.globalData.windowHeight, 
       windowWidth:app.globalData.windowWidth
       });
-    checklogin(that);
+    util.checklogin(that,app);
   },
 
 
@@ -65,6 +70,28 @@ Page({
       urls: [current] // 需要预览的图片http链接列表  
     })
   },
+
+  changeEdit: function (e) {
+    this.setData({ isEdit: !this.data.isEdit });
+    if (this.data.isEdit) {
+      this.setData({ editImage: "../../image/done.png" })
+      this.setData({ showTopTips: true, errorMsg: '修改模式，点击内容修改，结束点击完成' });
+    } else {
+      this.setData({ editImage: "../../image/edit.png" })
+      this.setData({ showTopTips: false, errorMsg: '修改完成' });
+    }
+    console.log(this.data.isEdit);
+
+  },
+  dataEdit: function (e) {
+    if (!(e.target.dataset.keyword == e.detail.value)) {
+      util.editStuInfo(e.target.dataset.xh, e.target.dataset.method, e.detail.value, e.target.dataset.keyword, e.target.dataset.xydh, this)
+      console.log(e.target.dataset.xh);
+    }
+    //console.log(e.target.dataset.xh);
+
+  },
+
 
   bindTouchStart: function (e) {
     this.startTime = e.timeStamp;
@@ -96,7 +123,7 @@ Page({
       var method = e.target.dataset.method;
       var keyword = e.target.dataset.keyword;
       console.log(keyword);
-      requestStuInfo(method, keyword, this);
+      util.requestStuInfo(method, keyword, this);
       console.log("长按");
       wx.showToast({
         title: "加载：" + keyword,
@@ -173,6 +200,7 @@ function processFileUploadForAsr(urls, filePath, self) {
     formData: { skey: skey, method: 'xm'},
     header: { 'content-type': 'multipart/form-data' },
     success: function (res) {
+      self.setData({ editImage: "../../image/edit.png", isEdit: false })
       var result=res.data;
       if (!isJSON(result) && (typeof result == 'string')) { console.log('返回结果错误' + result); self.setData({ showTopTips: true, errorMsg: '服务器错误，请重试' });return; }
       var result =JSON.parse(result);
@@ -201,7 +229,7 @@ function processFileUploadForAsr(urls, filePath, self) {
           } catch (e) {
             // Do something when catch error
           }
-          checklogin(self);
+          util.checklogin(self.app);
 
         }
         self.setData({ hiddenData: true });
@@ -226,152 +254,6 @@ function processFileUploadForAsr(urls, filePath, self) {
       wx.hideToast();
     }
   });
-}
-
-
-function requestStuInfo(method, keyword, self) {
-
-  var skey = wx.getStorageSync('user[skey]');
-  console.log(method + ',' + keyword);
-  var urls = url + "/admin/pub/xcxapi.html";//查询数据的URL 
-  wx.request({
-    url: urls,
-    data: { keyword: keyword, skey: skey, method: method },
-    method: 'GET',
-    success: function (res) {
-      var result = res.data;
-      console.log(result);
-      //校验返回数据，若不是JSON数据则返回
-      if (!isJSON(result)&&(typeof result=='string')){ console.log('返回结果错误'); return; }
-      if (typeof (result.error_code) == "undefined") {
-        var data = result.students;
-        //var jsonData = JSON.stringify(data);
-        //typeof cb == "function" && cb(true, jsonData)
-        typeof self !== 'undefined' && self.setData({
-          outputTxt: data
-        })
-        var query = wx.createSelectorQuery();
-        typeof self !== 'undefined' && self.setData({
-          scrolltop: 0
-        })
-        self.setData({ showTopTips: false, errorMsg: '' });
-        wx.showToast({
-          title: keyword + " OK！",
-          icon: 'success',
-          duration: 2000
-        })
-      } else {
-        if (result.error_code == '002') {
-          try {
-            wx.removeStorageSync('user[skey]');
-          } catch (e) {
-            // Do something when catch error
-          }
-          checklogin(self);
-
-        }
-        self.setData({ hiddenData: true });
-        self.setData({ showTopTips: true, errorMsg: result.error_message });
-        wx.showToast({
-          title: result.error_message,
-          image: '../../image/ava_error.png',
-          duration: 2000
-        })
-        return;
-      }
-    },
-    fail: function (e) {
-      var toastText = '获取数据失败' + JSON.stringify(e);
-      self.setData({
-        hiddenLoading: !self.data.hiddenLoading,
-        hiddenData: true
-      });
-      wx.showToast({
-        title: toastText,
-        icon: '',
-        duration: 2000
-      })
-    },
-    complete: function () {
-      // complete  
-    }
-  })
-}
-
-//检查登录状态
-function checklogin(self) {
-  var skey = wx.getStorageSync('user[skey]')
-  var openid = wx.getStorageSync('user[openid]')
-  var expired_time = wx.getStorageSync('user[expired_time]')
-  var timestamp = (Date.parse(new Date())) / 1000;
-  if (skey != '' && timestamp < expired_time) {
-    return;
-  }
-  login(self);
-}
-
-//重新获取登录状态
-function login(self) {
-  var timeflag = Date.parse(new Date());
-  wx.showLoading({
-    title: '登录验证中',
-    mask: true
-  })
-  // 因为我需要登录后的用户信息,但是app.getUserInfo和下面的request请求基本上是同时请求的所以获取不到  
-  app.mycheck();
-  // 在这里我设置了一个定时器循环多次去执行去判断上一步的函数执行完毕没有  
-  // 但是也不能无限循环,所以要叫一个判断当执行超过多少秒后报一个网络错误  
-  var times = setInterval(function () {
-    // 因为一开始缓存当中指定的key为假当为真的时候就说明上一步成功了这时候就可以开始发送下一步的请求了  
-    try {
-      var skey = wx.getStorageSync('user[skey]')
-    } catch (e) {
-
-    }
-
-    try {
-      var openid = wx.getStorageSync('user[openid]')
-    } catch (e) {
-
-    }
-
-    try {
-      var expired_time = wx.getStorageSync('user[expired_time]')
-    } catch (e) {
-
-    }
-    var timestamp = (Date.parse(new Date())) / 1000;
-    if (skey != '' && timestamp < expired_time) {
-      // 在这里停止加载的提示框  
-      setTimeout(function () {
-        wx.hideLoading()
-      }, 1000)
-      // 这里必须要清除不然就等着循环死吧  
-      clearTimeout(times);
-
-      self.setData({ showTopTips: false, errorMsg: '' });
-
-      wx.showToast({
-        title: '验证成功',
-        icon: 'success',
-        duration: 1500
-      })
-    } else {
-      if (Date.parse(new Date()) > (timeflag + 8000)) {
-        setTimeout(function () {
-          wx.hideLoading()
-        }, 1000)
-        // 这里必须要清除不然就等着循环死吧  
-        clearTimeout(times);
-        wx.showToast({
-          title: '登录验证失败',
-          image: '../../image/ava_error.png',
-          duration: 1500
-        })
-      }
-
-    }
-  }, 1000);
 }
 
 //判断反悔结果是否JSON字符串
--- utils/util.js
+++ utils/util.js
@@ -1,89 +1,19 @@
 //获取应用实例
-var app = getApp()
+//var app = require('../app.js');
 
 var Guid = require('./GUID.js');
 
 var uuidSaved = '';
-
-//将date格式的数据转成  hh:mm:ss 字符串格式
-function formatTime(date) {
-  var year = date.getFullYear()
-  var month = date.getMonth() + 1
-  var day = date.getDate()
-
-  var hour = date.getHours()
-  var minute = date.getMinutes()
-  var second = date.getSeconds()
-
-
-  //return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-  return [hour, minute, second].map(formatNumber).join(':')
-}
-
-function formatNumber(n) {
-  n = n.toString()
-  return n[1] ? n : '0' + n
-}
-
-
-function req(url, data, cb) {
-  wx.request({
-    url: url,
-    data: data,
-    method: 'post',
-    header: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    success: function (res) {
-      return typeof cb == "function" && cb(res.data)
-    },
-    fail: function () {
-      return typeof cb == "function" && cb(false)
-    }
-  })
-}
-
-function getReq(url, data, cb) {
-  wx.request({
-    url: url,
-    data: data,
-    method: 'get',
-    header: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    success: function (res) {
-      return typeof cb == "function" && cb(res.data)
-    },
-    fail: function () {
-      return typeof cb == "function" && cb(false)
-    }
-  })
-}  
-
+var url ='https://zhxg.tzc.edu.cn';
 // 去前后空格 
 function trim(str) {
   return str.replace(/(^\s*)|(\s*$)/g, "")
-}
-// 提示错误信息  
-function isError(msg, that) {
-  that.setData({
-    showTopTips: true,
-    errorMsg: msg
-  })
-}  
-// 清空错误信息  
-function clearError(that) {
-  that.setData({
-    showTopTips: false,
-    errorMsg: ""
-  })
 }
 
 
 //log接口封装
 function log(obj) {
-  console.log('<' + formatTime(new Date(Date.now())) + '>' + ' ' + obj)
-
-  //往用户缓存去写
-  // var logs = wx.getStorageSync('logs') || []
-  // logs.unshift('[' + formatTime(new Date(Date.now())) + ']' + ' ' + obj)
-  // wx.setStorageSync('logs', logs)
+  console.log(obj)
 }
 
 //获取用户唯一标识，NLI接口中要上传用户唯一标识，这里获取：第一次登录时生成的uuid+微信号所在地+昵称
@@ -107,33 +37,239 @@ function getUserUnique(userInfo) {
   return unique;
 }
 
-//由 毫秒数字符串 获取  HH:mm:ss 格式时间
-function getHHMMSS(dateStr) {
-  var dateVal = new Date(parseInt(dateStr));
-  var hh = dateVal.getHours()
-  var mm = dateVal.getMinutes()
-  var ss = dateVal.getSeconds()
 
-  return ((hh >= 10) ? hh : '0' + hh) + ':' + ((mm >= 10) ? mm : '0' + mm) + ':' + ((ss >= 10) ? ss : '0' + ss)
+function requestStuInfo(method, keyword, self,app) {
+
+  //隐藏中间帮助提示tips框
+  typeof self !== 'undefined' && self.setData({
+    showHelpTips: false
+  })
+
+  var skey = wx.getStorageSync('user[skey]');
+  console.log(method + ',' + keyword);
+  var urls = url + "/admin/pub/xcxapi.html";//查询数据的URL 
+  wx.request({
+    url: urls,
+    data: { keyword: keyword, skey: skey, method: method },
+    method: 'GET',
+    success: function (res) {
+      self.setData({ editImage: "../../image/edit.png", isEdit: false })
+      var result = res.data;
+      console.log(result);
+      if (typeof (result.error_code) == "undefined") {
+        var data = result.students;
+        //var jsonData = JSON.stringify(data);
+        //typeof cb == "function" && cb(true, jsonData)
+        console.log(data);
+        typeof self !== 'undefined' && self.setData({
+          outputTxt: data
+        })
+        var query = wx.createSelectorQuery();
+        typeof self !== 'undefined' && self.setData({
+          scrolltop: 0
+        })
+        self.setData({ showTopTips: false, errorMsg: '' });
+        wx.showToast({
+          title: keyword + " OK！",
+          icon: 'success',
+          duration: 2000
+        })
+      } else {
+        if (result.error_code == '002') {
+          try {
+            wx.removeStorageSync('user[skey]');
+          } catch (e) {
+            // Do something when catch error
+          }
+          checklogin(self,app);
+
+        }
+        self.setData({ hiddenData: true });
+        self.setData({ showTopTips: true, errorMsg: result.error_message });
+        wx.showToast({
+          title: result.error_message,
+          image: '../../image/ava_error.png',
+          duration: 2000
+        })
+        return;
+      }
+    },
+    fail: function (e) {
+      var toastText = '获取数据失败' + JSON.stringify(e);
+      self.setData({
+        hiddenLoading: !self.data.hiddenLoading,
+        hiddenData: true
+      });
+      wx.showToast({
+        title: toastText,
+        icon: '',
+        duration: 2000
+      })
+    },
+    complete: function () {
+      // complete  
+    }
+  })
 }
 
 
-//从语料数组中随机挑选一条语料
-function getRandomItem(corpusList) {
-  var ret = corpusList[0];
-  ret = corpusList[Math.floor(Math.random() * corpusList.length)];
-  return ret;
+//修改学生信息数据
+function editStuInfo(xh, method, keyword, oldkeyword, xydh, self,app) {
+
+  var skey = wx.getStorageSync('user[skey]');
+  var urls = url + "/admin/pub/xcxeditapi.html";//查询数据的URL 
+  wx.request({
+    url: urls,
+    data: { xh: xh, keyword: keyword, oldkeyword: oldkeyword, skey: skey, method: method, xydh: xydh },
+    method: 'GET',
+    success: function (res) {
+      var result = res.data;
+      console.log(result);
+      if (typeof (result.error_code) == "undefined") {
+        self.setData({ showTopTips: false, errorMsg: '' });
+        wx.showToast({
+          title: result.message,
+          icon: 'success',
+          duration: 2000
+        })
+        requestStuInfo('xh', xh, self);
+      } else {
+        if (result.error_code == '002') {
+          try {
+            wx.removeStorageSync('user[skey]');
+          } catch (e) {
+            // Do something when catch error
+          }
+          checklogin(self,app);
+
+        }
+        self.setData({ hiddenData: true });
+        self.setData({ showTopTips: true, errorMsg: result.error_message });
+        wx.showToast({
+          title: result.error_message,
+          image: '../../image/ava_error.png',
+          duration: 2000
+        })
+        return;
+      }
+    },
+    fail: function (e) {
+      var toastText = '获取数据失败' + JSON.stringify(e);
+      self.setData({
+        hiddenLoading: !self.data.hiddenLoading,
+        hiddenData: true
+      });
+      wx.showToast({
+        title: toastText,
+        icon: '',
+        duration: 2000
+      })
+    },
+    complete: function () {
+      // complete  
+    }
+  })
 }
+
+//检查登录状态
+function checklogin(self,app) {
+  var skey = wx.getStorageSync('user[skey]')
+  var openid = wx.getStorageSync('user[openid]')
+  var expired_time = wx.getStorageSync('user[expired_time]')
+  var group = wx.getStorageSync('user[group]')
+  var timestamp = (Date.parse(new Date())) / 1000;
+  if (skey != '' && timestamp < expired_time) {
+    if (group == "管理员" || group == "admin") {
+      self.setData({ isAdmin: true })
+    }
+    return;
+  }
+  login(self,app);
+}
+
+//重新获取登录状态
+function login(self,app) {
+  var timeflag = Date.parse(new Date());
+  wx.showLoading({
+    title: '登录验证中',
+    mask: true
+  })
+  // 因为我需要登录后的用户信息,但是app.getUserInfo和下面的request请求基本上是同时请求的所以获取不到  
+  app.mycheck();
+  // 在这里我设置了一个定时器循环多次去执行去判断上一步的函数执行完毕没有  
+  // 但是也不能无限循环,所以要叫一个判断当执行超过多少秒后报一个网络错误  
+  var times = setInterval(function () {
+    // 因为一开始缓存当中指定的key为假当为真的时候就说明上一步成功了这时候就可以开始发送下一步的请求了  
+    try {
+      var skey = wx.getStorageSync('user[skey]')
+    } catch (e) {
+
+    }
+
+    try {
+      var openid = wx.getStorageSync('user[openid]')
+    } catch (e) {
+
+    }
+
+    try {
+      var expired_time = wx.getStorageSync('user[expired_time]')
+    } catch (e) {
+
+    }
+
+    try {
+      var group = wx.getStorageSync('user[group]')
+    } catch (e) {
+
+    }
+
+    if (group == "管理员" || group == "admin") {
+      self.setData({ isAdmin: true })
+    }
+
+    var timestamp = (Date.parse(new Date())) / 1000;
+    console.log('开始判断成功没');
+    if (skey != '' && timestamp < expired_time) {
+      // 在这里停止加载的提示框  
+      setTimeout(function () {
+        wx.hideLoading()
+      }, 1000)
+      // 这里必须要清除不然就等着循环死吧  
+      clearTimeout(times);
+
+      self.setData({ showTopTips: false, errorMsg: '' });
+
+      wx.showToast({
+        title: '验证成功',
+        icon: 'success',
+        duration: 1500
+      })
+    } else {
+      if (Date.parse(new Date()) > (timeflag + 8000)) {
+        setTimeout(function () {
+          wx.hideLoading()
+        }, 1000)
+        // 这里必须要清除不然就等着循环死吧  
+        clearTimeout(times);
+        wx.showToast({
+          title: '登录验证失败',
+          image: '../../image/ava_error.png',
+          duration: 1500
+        })
+      }
+
+    }
+  }, 1000);
+}
+
 
 module.exports = {
   trim:trim,
-  isError: isError,
-  clearError: clearError,
-  req: req,
-  getReq: getReq,
-  formatTime: formatTime,
   log: log,
   getUserUnique: getUserUnique,
-  getHHMMSS: getHHMMSS,
-  getRandomItem: getRandomItem,
+  requestStuInfo: requestStuInfo,
+  editStuInfo: editStuInfo,
+  checklogin:checklogin,
+  login:login
 }
